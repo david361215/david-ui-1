4c25bfff78b7cc458bb4ed8bdc3401c2daa9ca44
david361215 <1138890119@qq.com>
2022-08-23 23:15:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# David UI for Vue 3
+
+## 库文件打包步骤
+
+1. 运行 yarn rollup -c
+2. 得到的 dist/lib 目录就是编译后的库文件所在目录
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./lib/gulu.scss";
+import "./index.scss";
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
